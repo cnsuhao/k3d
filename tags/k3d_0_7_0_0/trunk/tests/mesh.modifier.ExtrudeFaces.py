18edbcb60be558c8a029eb45e458bf67b14982cd
tshead <tshead@6c3b9138-102f-0410-aba5-b8adf0b7bd61>
2007-07-18 05:08:00 +0000

    COMP: Tagged K-3D Version 0.7.0.0

---
+++ tags/k3d_0_7_0_0/trunk/tests/mesh.modifier.ExtrudeFaces.py
@@ -0,0 +1,17 @@
+#python
+
+import k3d
+import testing
+
+setup = testing.setup_mesh_modifier_test("PolyGrid", "ExtrudeFaces")
+
+setup.source.rows = 3
+setup.source.columns = 3
+
+mesh_selection = k3d.deselect_all()
+mesh_selection.faces = [(4, 5, 1)]
+setup.modifier.mesh_selection = mesh_selection
+setup.modifier.distance = 5
+
+testing.mesh_comparison(setup.document, setup.modifier.get_property("output_mesh"), "mesh.modifier.ExtrudeFaces", 0)
+
