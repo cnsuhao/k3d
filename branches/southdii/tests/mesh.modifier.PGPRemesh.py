8818c5fa2ef0cb4506111b909dc7b18cefed3c79
tshead <tshead@6c3b9138-102f-0410-aba5-b8adf0b7bd61>
2007-08-07 05:37:13 +0000

    COMP: Update the PGPRemesh test to triangulate the input torus

--- branches/southdii/tests/mesh.modifier.PGPRemesh.py
+++ branches/southdii/tests/mesh.modifier.PGPRemesh.py
@@ -3,6 +3,6 @@
 import k3d
 import testing
 
-setup = testing.setup_mesh_modifier_test("PolyTorus", "PGPRemesh")
-testing.mesh_comparison(setup.document, setup.modifier.get_property("output_mesh"), "mesh.modifier.PGPRemesh", 1)
+setup = testing.setup_two_mesh_modifier_test("PolyTorus", "TriangulateFaces", "PGPRemesh")
+testing.mesh_comparison(setup.document, setup.modifier2.get_property("output_mesh"), "mesh.modifier.PGPRemesh", 1)
 
